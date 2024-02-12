4be98007308cd8e06c287bcaffbef425c434f067
omarramy22 <omarramy@aucegypt.edu>
2024-02-12 13:26:53 +0200

    message

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "function1.h"
+#include "function2.h"
+#include "function3.h"
+#include "function4.h"
+#include <iostream>
+using namespace std;
+int main ()
+{
+int N;
+cout << "Enter the value of N: ";
+cin >> N;
+checkSolution(N);
+return 0;
+}
+
